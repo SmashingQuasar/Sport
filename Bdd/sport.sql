3c702a186538c5b159b6b3b85ccae86381e86d79
e.bismuth <e.bismuth@agence-senzo.fr>
2019-03-14 11:06:20 +0100

    new bdd

--- Bdd/sport.sql
+++ Bdd/sport.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.3
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
 --
--- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 12 mars 2019 à 13:11
--- Version du serveur :  5.7.23
--- Version de PHP :  7.2.10
+-- Client :  localhost
+-- Généré le :  Jeu 14 Mars 2019 à 10:05
+-- Version du serveur :  5.7.11
+-- Version de PHP :  7.2.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -28,28 +26,28 @@ SET time_zone = "+00:00";
 -- Structure de la table `adherents`
 --
 
-DROP TABLE IF EXISTS `adherents`;
-CREATE TABLE IF NOT EXISTS `adherents` (
-  `id_adherent` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `adherents` (
+  `id_adherent` int(11) NOT NULL,
   `prenom` varchar(45) NOT NULL,
   `nom` varchar(45) NOT NULL,
   `date_naissance` date NOT NULL,
-  `genre` enum('M','F') NOT NULL,
-  PRIMARY KEY (`id_adherent`)
-) ENGINE=MyISAM AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
+  `genre` enum('M','F') NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `adherents`
+-- Contenu de la table `adherents`
 --
 
 INSERT INTO `adherents` (`id_adherent`, `prenom`, `nom`, `date_naissance`, `genre`) VALUES
-(1, 'elie', 'bismuth', '2019-03-19', 'M'),
-(2, 'bryan', 'ohayon', '2019-02-19', 'M'),
-(3, 'alan', 'toledano', '2019-03-05', 'M'),
-(4, 'yael', 'sebag', '2019-03-05', 'F'),
-(5, 'joyce', 'elmaleh', '2019-01-14', 'F'),
-(12, 'Jhon', 'Doe', '2000-02-19', 'M'),
-(11, 'Harry', 'Said', '1996-02-28', 'M');
+(1, 'elie', 'bismuth', '1995-03-19', 'M'),
+(2, 'bryan', 'ohayon', '1997-02-19', 'M'),
+(3, 'alan', 'toledano', '2005-03-05', 'M'),
+(4, 'yael', 'sebag', '2003-03-05', 'F'),
+(5, 'joyce', 'elmaleh', '1920-01-14', 'F'),
+(12, 'Jhon', 'Doe', '1990-02-19', 'M'),
+(11, 'Harry', 'Said', '1996-02-28', 'M'),
+(22, 'Rafael', 'Attias', '1960-02-18', 'M'),
+(19, 'Jhon', 'Doe', '2002-03-28', 'M');
 
 -- --------------------------------------------------------
 
@@ -57,18 +55,15 @@ INSERT INTO `adherents` (`id_adherent`, `prenom`, `nom`, `date_naissance`, `genr
 -- Structure de la table `adherents_est_inscrit`
 --
 
-DROP TABLE IF EXISTS `adherents_est_inscrit`;
-CREATE TABLE IF NOT EXISTS `adherents_est_inscrit` (
+CREATE TABLE `adherents_est_inscrit` (
   `id_adherent` int(11) NOT NULL,
   `id_club` int(11) NOT NULL,
   `date_inscription` date NOT NULL,
-  `annee_de_licence` varchar(4) NOT NULL,
-  PRIMARY KEY (`id_adherent`,`id_club`),
-  KEY `id_club` (`id_club`)
+  `annee_de_licence` varchar(4) NOT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `adherents_est_inscrit`
+-- Contenu de la table `adherents_est_inscrit`
 --
 
 INSERT INTO `adherents_est_inscrit` (`id_adherent`, `id_club`, `date_inscription`, `annee_de_licence`) VALUES
@@ -81,7 +76,9 @@ INSERT INTO `adherents_est_inscrit` (`id_adherent`, `id_club`, `date_inscription
 (5, 4, '2019-03-05', '1994'),
 (4, 3, '2019-03-05', '2017'),
 (12, 5, '2019-03-10', '2015'),
-(11, 2, '2019-03-10', '2015');
+(11, 2, '2019-03-10', '2015'),
+(21, 1, '2019-03-13', '1996'),
+(22, 5, '2019-03-14', '1995');
 
 -- --------------------------------------------------------
 
@@ -89,17 +86,15 @@ INSERT INTO `adherents_est_inscrit` (`id_adherent`, `id_club`, `date_inscription
 -- Structure de la table `clubs`
 --
 
-DROP TABLE IF EXISTS `clubs`;
-CREATE TABLE IF NOT EXISTS `clubs` (
-  `id_club` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `clubs` (
+  `id_club` int(11) NOT NULL,
   `nom_club` varchar(45) NOT NULL,
   `code_postal` varchar(45) NOT NULL,
-  `ville` varchar(45) NOT NULL,
-  PRIMARY KEY (`id_club`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `ville` varchar(45) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
--- Déchargement des données de la table `clubs`
+-- Contenu de la table `clubs`
 --
 
 INSERT INTO `clubs` (`id_club`, `nom_club`, `code_postal`, `ville`) VALUES
@@ -108,8 +103,44 @@ INSERT INTO `clubs` (`id_club`, `nom_club`, `code_postal`, `ville`) VALUES
 (3, 'Les skieurs du dimanche', '76588', 'Dijon'),
 (4, 'ToutSchuss', '32000', 'Perpignan'),
 (5, 'SkieursToutTerrain', '75000', 'Paris');
-COMMIT;
 
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `adherents`
+--
+ALTER TABLE `adherents`
+  ADD PRIMARY KEY (`id_adherent`);
+
+--
+-- Index pour la table `adherents_est_inscrit`
+--
+ALTER TABLE `adherents_est_inscrit`
+  ADD PRIMARY KEY (`id_adherent`,`id_club`),
+  ADD KEY `id_club` (`id_club`);
+
+--
+-- Index pour la table `clubs`
+--
+ALTER TABLE `clubs`
+  ADD PRIMARY KEY (`id_club`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `adherents`
+--
+ALTER TABLE `adherents`
+  MODIFY `id_adherent` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+--
+-- AUTO_INCREMENT pour la table `clubs`
+--
+ALTER TABLE `clubs`
+  MODIFY `id_club` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
